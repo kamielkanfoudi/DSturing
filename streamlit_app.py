d120a2ba3b4b9cc1406411e42ea8775f6abd2132
kamielkanfoudi <99194700+kamielkanfoudi@users.noreply.github.com>
2022-02-14 19:34:52 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,9 @@
 import streamlit as st
-import plotly as 
+import plotly.express as px
 
 st.title('An Overview of the Sicilian Defense.')
 
 
-st.text("“Those who say they understand chess, understand nothing.” – Robert Hübner
+st.text(" “Those who say they understand chess, understand nothing.” – Robert Hübner")
 
-"One thing we can learn to understand, though, is data. And, with data, I want to explore one of chess’s most famed names: The Sicilian defense.") 
+st.text("One thing we can learn to understand, though, is data. And, with data, I want to explore one of chess’s most famed names: The Sicilian defense.") 
