b83c7d50e3b9996bc279551eef24b0476d7b9c05
kamielkanfoudi <99194700+kamielkanfoudi@users.noreply.github.com>
2022-02-14 20:09:41 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,9 +8,9 @@ st.title('An Overview of the Sicilian Defense.')
 
 st.text(" “Those who say they understand chess, understand nothing.” – Robert Hübner")
 
-st.text("One thing we can learn to understand, though, is data. And, with data, I want to explore one of chess’s most famed names: The Sicilian defense! ") 
+st.write("One thing we can learn to understand, though, is data. And, with data, I want to explore one of chess’s most famed names: The Sicilian defense! ") 
 
-st.text("Firstly, a quick overview of two chess tournaments held on one of the most prominent chess platforms, lichess. We scraped data of two tournaments held in January 2022: one consisting of only titled players and one open to all chess players. With almost 2000 games to analyze, our first step was to start at the beginning. How do the players tend to start? Put simply, the foundational mechanism of any chess game is known as the opening. Opening theory intends to document most, if not all, possible sets of opening plays in the ECO (encyclopedia of chess Openings)" 
+st.write("Firstly, a quick overview of two chess tournaments held on one of the most prominent chess platforms, lichess. We scraped data of two tournaments held in January 2022: one consisting of only titled players and one open to all chess players. With almost 2000 games to analyze, our first step was to start at the beginning. How do the players tend to start? Put simply, the foundational mechanism of any chess game is known as the opening. Opening theory intends to document most, if not all, possible sets of opening plays in the ECO (encyclopedia of chess Openings)" 
 
 trackingdata = pd.read_csv('trackingmoves1.csv')
 
