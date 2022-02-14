e06559b8db9e209eb47c00a566c0049d518fbed7
kamielkanfoudi <99194700+kamielkanfoudi@users.noreply.github.com>
2022-02-14 20:02:38 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,8 +8,9 @@ st.title('An Overview of the Sicilian Defense.')
 
 st.text(" “Those who say they understand chess, understand nothing.” – Robert Hübner")
 
-st.text("One thing we can learn to understand, though, is data. And, with data, I want to explore one of chess’s most famed names: The Sicilian defense.") 
+st.text("One thing we can learn to understand, though, is data. And, with data, I want to explore one of chess’s most famed names: The Sicilian defense!") 
 
 trackingdata = pd.read_csv('trackingmoves1.csv')
 
 st.line_chart(trackingdata)
+
